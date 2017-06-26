89c94671af5eca3396ceb2a569f0514c351bf657
NEW <aufcl4858@naver.com>
2017-06-26 22:53:03 +0900

    Update admin settion
    1. 관리 페이지 세팅 완료
    - Question과 Choice 모델 추가

--- polls_app/mysite/polls/views.py
+++ polls_app/mysite/polls/views.py
@@ -7,4 +7,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
- return HttpResponse("Hello, world. You're at the polls index.")
+    return HttpResponse("Hello, world. You're at the polls index.")
+
+def results(request, question_id):
+    responce = "Question : %s"
+    return HttpResponse(responce % question_id)
