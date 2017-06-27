6f2e1ad39398386a160c68ce2d248389359c8ab4
NEW <aufcl4858@naver.com>
2017-06-27 21:44:50 +0900

    Update image upload and view decoration
    1. 이미지 업로드 기능 추가
    2. 메인 뷰 데코레이션
    - 이미지 출력
    - 2개씩 출력되도록 수정

--- kilogram_app/mysite/kilogram/urls.py
+++ kilogram_app/mysite/kilogram/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import url
 from django.contrib import admin
+from django.contrib.auth.decorators import login_required
 from . import views
 
 app_name ='kilogram'
 
 urlpatterns = [
-    url(r'^$', views.IndexView.as_view(), name = 'index'),
+    url(r'^$', login_required(views.IndexView.as_view()), name = 'index'),
+    url(r'^upload$', views.upload, name='upload')
 ]
--- kilogram_app/mysite/kilogram/views.py
+++ kilogram_app/mysite/kilogram/views.py
@@ -1,16 +1,37 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic.base import TemplateView
+from django.views.generic.list import ListView
 from django.views.generic.edit import CreateView
 from django.core.urlresolvers import reverse_lazy
+from django.contrib.auth.decorators import login_required
 
-from .forms import CreateUserForm
+from .forms import CreateUserForm, UploadForm
 # Create your views here.
 
-class IndexView(TemplateView):
-    template_name = 'kilogram/index.html'
+@login_required
+def upload(request):
+    if request.method == "POST":
+        form = UploadForm(request.POST, request.FILES)
+        if form.is_valid():
+            photo = form.save(commit=False)
+            photo.owner = request.user
+            form.save()
+            return redirect('kilogram:index')
+
+    form = UploadForm
+    return render(request, 'kilogram/upload.html', {'form':form})
+
+class IndexView(ListView):
+    # model = Photo
+    context_object_name = 'user_photo_list'
+    paginate_by = 2
+
+    def get_queryset(self):
+        user = self.request.user
+        return user.photo_set.all().order_by('-pub_date')
 
 class CreateUserView(CreateView):
     template_name = 'registration/signup.html'
